c7cabacb14d895400fddc1eb412f4e139aa8ae6b
Nine <alvedin.gasal@gmail.com>
2024-08-16 20:53:41 +0200

    Section 3 - API Architecture

---
+++ Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,26 @@
+using Core.Entities;
+
+namespace Core.Interfaces;
+
+public interface IProductRepository
+{
+    Task<IReadOnlyList<Product>> GetProductsAsync(string? brand, string? category, string? type, string? sort);
+
+    Task<Product?> GetProductByIdAsync(int id);
+
+    Task<IReadOnlyList<string>> GetBrandsAsync();
+
+    Task<IReadOnlyList<string>> GetCategoriesAsync();
+
+    Task<IReadOnlyList<string>> GetTypesAsync();
+
+    void AddProduct(Product product);
+
+    void UpdateProduct(Product product);
+
+    void DeleteProduct(Product product);
+
+    bool ProductExists(int id);
+
+    Task<bool> SaveChangesAsync();
+}
